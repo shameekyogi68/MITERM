8001c1ba48b3b03e375b8c5d852efe183e9205c0
Shameek Yogi <shameekyogiofficial@gmail.com>
2026-06-07 23:47:49 +0530

    chore: update seed script connection mode

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,5 @@
 import 'dotenv/config';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import { prisma } from '../lib/prisma';
 
 async function main() {
   const members = [
